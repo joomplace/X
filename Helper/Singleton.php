bcedc6b17f038ba10a352bc9743be7168b9af286
Alexandr Kosarev <alex7r@ya.ru>
2019-03-24 01:56:53 +0300

    implement singleton trait

--- Helper/Singleton.php
+++ Helper/Singleton.php
@@ -1,5 +1,9 @@
 <?php
 /**
+ * Copyright (c) 2019. Alexandr Kosarev, @kosarev.by
+ */
+
+/**
  * Created by PhpStorm.
  * User: Alexandr
  * Date: 24.03.2019
@@ -11,5 +15,22 @@ namespace Joomplace\X\Helper;
 
 trait Singleton
 {
+    private static $instance = null;
 
+    public static function getInstance()
+    {
+        if (null === self::$instance)
+        {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    private function __clone() {
+        parent::__clone();
+    }
+
+    private function __construct() {
+        parent::__construct();
+    }
 }
